8f9aa6713d40a4f8f883405be356da178339aebf
dendinge21 <dendinge21@up.edu>
2019-02-19 20:58:40 -0800

    Added comments on what we need to do

---
+++ app/src/main/java/com/example/homework4d/CosmicWimpoutState.java
@@ -0,0 +1,31 @@
+package com.example.homework4d;
+
+public class CosmicWimpoutState {
+    //instance variables that should encompass all the information
+    /**
+     * IDEAS
+     * Whose turn it is? (player)
+     * Current score of each player (int)
+     * current state of dice (create a dice class?)
+     *
+     */
+
+    //implement a constructor that initializes all the variables
+
+    //implement a deep copy
+
+    /**
+    *toString(); describes the state of a game as a string, prints all the values
+     * of all the variables; add @Override tag
+    */
+
+    /**
+     add methods for each of the actions - each should have a boolean return value;
+     when called, each method should verify the move is a legal move for the current
+     game state
+     return false; if its illegal
+     return true; if its legal (modify the game state if its true \
+     pass in player as a parameter
+     */
+
+}
